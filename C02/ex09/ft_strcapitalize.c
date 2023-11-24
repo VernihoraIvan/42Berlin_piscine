5fd25fe7258ab79ba278715827e246896aac1cbb
VernihoraIvan <vernihorai@gmail.com>
2023-11-24 12:36:26 +0100

    C02 project done

---
+++ C02/ex09/ft_strcapitalize.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iverniho <iverniho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 13:53:51 by iverniho          #+#    #+#             */
+/*   Updated: 2023/10/04 17:59:12 by iverniho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int		i;
+	int		m;
+
+	i = 0;
+	m = 1;
+	ft_strlowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			if (m == 1)
+				str[i] -= 32;
+			m = 0;
+		}
+		else if (str[i] >= 48 && str[i] <= 57)
+			m = 0;
+		else
+			m = 1;
+		i++;
+	}
+	return (str);
+}
